2c4cd6aed00b17d285a68d438ff44c2d73e998f8
7Winds <nshusa99@gmail.com>
2015-09-03 12:40:11 -0500

    Added documentation

--- src/core/Configuration.java
+++ src/core/Configuration.java
@@ -1,7 +1,14 @@
 package core;
 
+/**
+ * The main configuration class for the server
+ * @author 7Winds
+ */
 public class Configuration {
 	
+	/**
+	 * Displays debug messages
+	 */
 	public static boolean server_debug = false;
 
 }
